390d45b4d250c5de75678c147c2a386c3c8bdb6e
Eddy Cos <cos_eddy_man@yahoo.com>
2025-04-05 02:06:36 +0300

    small tweaks

--- extractPrivateCartData.py
+++ extractPrivateCartData.py
@@ -1,9 +1,8 @@
 import requests
-import json
 import os
 
 
-def get_cart_private_data(url, auth_token):
+def get_cart_private_data(url, auth_token, cart_name):
     # Headers (excluding sensitive info)
     headers = {"accept": "application/json, text/plain, */*",
                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
@@ -24,8 +23,11 @@ def get_cart_private_data(url, auth_token):
         # Create the output folder if it doesn't exist
         os.makedirs("output", exist_ok=True)
 
+        # Create filepath
+        file_to_write_to_path = "output/" + cart_name + ".txt"
+
         # Open the file to write data
-        with open("output/exported_cart_data.txt", "w") as file:
+        with open(file_to_write_to_path, "w") as file:
             # Loop through each item and write the required details into the file
             for index, item in enumerate(items, start=1):
                 # Get the artist names and join them with a comma
@@ -34,6 +36,6 @@ def get_cart_private_data(url, auth_token):
                 mix_name = item['item']['mix_name']  # Get the mix name
                 file.write(f"{index} | {artists} | {title} | {mix_name}\n")
 
-        print("Data exported to 'output/exported_cart_data.txt'")  # Confirm success
+        print(f"Data exported to {file_to_write_to_path}")  # Confirm success
     else:
         print(f"Error: {response.status_code}, {response.text}")
--- run.py
+++ run.py
@@ -9,8 +9,11 @@ def __main__():
     # give publicly accessible url as param
     # extract_using_request("https://www.beatport.com/genre/organic-house/93/top-100")
 
+    # Enter Cart Name
+    cart_name = "2025_March_1"
+
     # Private authorization token, edit manually
-    token = "REPLACE WITH YOUR OWN"
+    token = "WRITE YOUR OWN TOKEN AUTH TOKEN HERE"
     # param1 is API url
-    get_cart_private_data("https://api.beatport.com/v4/my/carts/284486641/items/tracks/?page=1&per_page=150", token)
+    get_cart_private_data("https://api.beatport.com/v4/my/carts/284486641/items/tracks/?page=1&per_page=150", token, cart_name)
 __main__()
